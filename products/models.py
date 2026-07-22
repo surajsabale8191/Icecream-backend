bdcbfb39c50a9087a478ee25bd9257906f239156
surajsabale8191 <surajsabale8191@gmail.com>
2026-07-22 12:08:27 +0530

    created cart model and migrated it

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Category(models.Model):
 
@@ -48,3 +48,30 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Cart(models.Model):
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(
+        default=1
+    )
+
+    added_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.name}"
